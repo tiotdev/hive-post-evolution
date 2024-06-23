6b5ab9ee1921d2a01ab322a6b9e9008878b1571c
tiotdev <tiotdev@tiot.de>
2024-06-24 00:15:22 +0200

    Fix mdx layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,5 @@
 import "@/styles/globals.css";
-import MDXLayout from "components/Docs/MDXLayout";
-import { Fragment } from "react";
 
 export default function App({ Component, pageProps }) {
-  const isMDXPage = Component.name === "MDXContent";
-  const Layout = isMDXPage ? MDXLayout : Fragment;
-
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  return <Component {...pageProps} />;
 }
