ace8ebd9ef0e0bf4ee9e9f1d87a984e75f923d1c
tiotdev <tiotdev@tiot.de>
2024-06-24 19:16:33 +0200

    Add header link

--- components/Docs/Header.js
+++ components/Docs/Header.js
@@ -1,12 +1,15 @@
 import React, { useContext } from "react";
 import { AuthContext } from "./AuthProvider";
+import Link from "next/link";
 
 const Header = () => {
   const { isLoggedIn, toggleLogin } = useContext(AuthContext);
 
   return (
     <header className="flex items-center justify-between bg-red-600 p-4">
-      <div className="text-xl font-bold text-white">Hive Post Evolution</div>
+      <div className="text-xl font-bold text-white">
+        <Link href="/">Hive Post Evolution</Link>
+      </div>
       <button
         onClick={toggleLogin}
         className="rounded bg-white px-4 py-2 font-semibold text-red-600 transition duration-300 hover:bg-red-100"
